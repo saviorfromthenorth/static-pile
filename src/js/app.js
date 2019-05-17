057ea7b773a39adc69b274f13c9f4398104fef72
andie valderama <3284158+saviorfromthenorth@users.noreply.github.com>
2019-05-17 12:06:21 +0800

    added elasticlunr to app.js

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,48 @@
 require('./settings');
 
+var card1 = {
+  id: 1,
+  title: 'The Quickness',
+  description: 'The quick brown fox jumps over the lazy dog',
+  tags: 'quick, brown, fox'
+};
+
+var card2 = {
+  id: 2,
+  title: 'Lorem Ipsum',
+  description: 'Lorem ipsum dolor sit amet',
+  tags: 'lorem, ipsum, dolor, sit, amet'
+};
+
+
 $(function() {
-	var player = new Plyr('#my-player');
+  var player = new Plyr('#my-player');
+
+  var index = elasticlunr(function() {
+    this.addField('title');
+    this.addField('description');
+    this.addField('tags');
+    this.setRef('id');
+  });
+
+  index.addDoc(card1);
+  index.addDoc(card2);
+
+  $('#search').on('propertychange input', function() {
+    if (this.value.length > 0) {
+      var results = index.search(this.value, {
+        fields: {
+          title: { boost: 3 },
+          description: { boost: 2 },
+          tags: { boost: 1 }
+        }
+      });
+
+      if (results.length > 0) {
+        for (var i in results) {
+          console.log(results[i].ref);
+        }
+      }
+    }
+  });
 });
--- src/js/settings.js
+++ src/js/settings.js
@@ -1,5 +1,6 @@
 window.$ = window.jQuery = require('jquery');
 window.Plyr = require('plyr');
+window.elasticlunr = require('elasticlunr');
 require('what-input');
 require('foundation-sites');
 
